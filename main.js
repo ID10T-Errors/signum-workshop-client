bb751695064e5049ea24a860b1ce1ab7bcd74e98
Zeb McCorkle <zmccorkle@classcoder.com>
2015-12-11 16:16:20 -0600

    Create sample app

---
+++ main.js
@@ -0,0 +1,8 @@
+const angular = require('angular')
+require('angular-route')
+
+const app = angular.module('signum', ['ng-route'])
+
+app.controller('HomePageController', ['$scope', function ($scope) {
+
+}])
