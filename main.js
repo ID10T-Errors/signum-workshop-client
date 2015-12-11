44a7413f7a5aa09ffe356f46103cd182c90ef7c5
Zeb McCorkle <zmccorkle@classcoder.com>
2015-12-11 17:19:37 -0600

    Add a demo app

--- main.js
+++ main.js
@@ -18,9 +18,33 @@ app.config(['$routeProvider', function ($routeProvider) {
         })
 }])
 
-app.controller('HomePageController', ['$scope', '$timeout', function ($scope, $timeout) {
+app.service('SectionService', [function () {
+    var sections = {
+        1: {
+            name: 'hello world'
+        }
+    }
+    return {
+        getSection: function (index) {
+            return sections[index]
+        },
+        setSection: function (index, value) {
+            sections[index] = value
+        },
+        getSections: function () {
+            return angular.copy(sections)
+        }
+    }
+}])
+
+app.controller('HomePageController', ['$scope', '$timeout', 'SectionService', function ($scope, $timeout, SectionService) {
     $scope.helloWorld = 'hi'
     $timeout(function () {
         $scope.helloWorld = ''
     }, 2000)
+    $scope.sections = SectionService.getSections()
+}])
+
+app.controller('SectionController', ['$scope', '$routeParams', 'SectionService', function ($scope, $routeParams, SectionService) {
+    $scope.section = SectionService.getSection($routeParams.section)
 }])
