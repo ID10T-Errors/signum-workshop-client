f169545f0226764742d3c3ccb51e78ece0667d1c
Zeb McCorkle <zmccorkle@classcoder.com>
2015-12-11 16:38:22 -0600

    All of the things
    don't even care

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const angular = require('angular')
-require('angular-route')
+const ngRoute = require('angular-route')
 
-const app = angular.module('signum', ['ng-route'])
+const app = angular.module('signum', [ngRoute])
 
-app.controller('HomePageController', ['$scope', function ($scope) {
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/section/:section', {
+            templateUrl: 'partials/section.html',
+            controller: 'SectionController'
+        })
+        .when('/home', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomePageController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        })
+}])
 
+app.controller('HomePageController', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.helloWorld = 'hi'
+    $timeout(function () {
+        $scope.helloWorld = ''
+    }, 2000)
 }])
