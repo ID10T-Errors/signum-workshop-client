4c9fde1dd162527d5ea80746a960344726b58100
Zeb McCorkle <zmccorkle@classcoder.com>
2015-12-14 19:58:52 -0600

    Add a getFileContents function to Repository
    Now I've got to add an `ls`-like function.

--- abstract/github.js
+++ abstract/github.js
@@ -18,20 +18,39 @@ class Repository {
       this.int_name = res.name
       this.int_description = res.description
       this.int_fullName = res.full_name
+      this.int_userName = res.owner.login
       cb(null, this)
     })
   }
 
-  get name() {
+  get name () {
     if (this.int_name == null) throw new ReferenceError('The name hasn\'t been defined yet. Has the constructor callback been called yet?')
     return this.int_name
   }
-  get description() {
+  get description () {
     if (this.int_description == null) throw new ReferenceError('The description hasn\'t been defined yet. Has the constructor callback been called yet?')
     return this.int_description
   }
-  get fullName() {
+  get fullName () {
     if (this.int_fullName == null) throw new ReferenceError('The full name hasn\'t been defined yet. Has the constructor callback been called yet?')
     return this.int_fullName
+  }
+  get userName () {
+    if (this.int_userName == null) throw new ReferenceError('The username hasn\'t been defined yet. Has the constructor callback been called yet?')
+    return this.int_userName
+
+  getFileContents (path, ref) {
+    return new Promise(function (resolve, reject) {
+      if (typeof path !== 'string') return reject(new TypeError('path must be a string.'))
+      github.repos.getContent({
+        user: this.repo.owner.login,
+        repo: this.userName,
+        path: path,
+        ref: ref
+      }, function (err, res) {
+        if (err) return reject(err)
+        resolve(res)
+      })
+    }
   }
 }
