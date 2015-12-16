0bedb9500d8411ecd8fb3adf111858d6a7ee3f8a
Zeb McCorkle <zmccorkle@classcoder.com>
2015-12-16 17:16:16 -0600

    Add the beginning of tabs
    NOT WORKING

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 require('./main.css')
 
+const async = require('async')
+
 const angular = require('angular')
 const ngRoute = require('angular-route')
 const ngMaterial = require('angular-material')
@@ -27,16 +29,58 @@ app.config(['$routeProvider', function ($routeProvider) {
             controller: 'HomePageController'
         })
         .otherwise({
-            redirectTo: '/home'
+            redirectTo: '/section/1/1'
         })
 }])
 
-app.service('SectionService', [function () {
-    var sections = {
-        1: {
-            name: 'hello world'
+app.service('SectionService', ['$rootScope', '$sce', function ($scope, $sce) {
+    var sections = [
+        {
+            title: 'Packet 1',
+            problems: []
         }
-    }
+    ]
+    var repo = new github.Repository('jdeans289', 'java-practice-problems', function (err) {
+        if (err) throw err
+        repo.ls('/CS1-packet1', 'gh-pages').then(function (files) {
+            var yamlFiles = []
+            for (let file of files) {
+                if (file.indexOf('.yml') > -1) {
+                    yamlFiles.push(file)
+                }
+            }
+            async.map(yamlFiles, function (file, callback) {
+                repo.catYaml('/CS1-packet1/' + file, 'gh-pages').then(function (contents) {
+                    var problem = {};
+                    try {
+                        problem.description = $sce.trustAsHtml(markdown.toHtml(contents.overview))
+                    } catch (e) {
+                        problem.description = $sce.trustAsHtml(contents.overview)
+                    }
+                    problem.template = 'class ' + (contents.filename.split('.')[0]) + ' {\n    \n}'
+                    problem.name = contents.title
+                    problem.filename = contents.filename
+                    console.log(problem)
+                    callback(null, problem)
+                }).catch(function (err) {
+                    console.error(err)
+                    callback()
+                })
+            }, function (err, problems) {
+                console.log('problems: ', problems)
+                var actualProblems = []
+                for (let problem of problems) {
+                    if (problem != null) actualProblems.push(problem)
+                }
+                sections[0].problems = actualProblems
+                for (let updator of updators) {
+                    updator()
+                }
+                console.log(actualProblems)
+            })
+        })
+    })
+    var updators = []
     return {
         getSection: function (index) {
             return sections[index]
@@ -46,8 +90,21 @@ app.service('SectionService', [function () {
         },
         getSections: function () {
             return angular.copy(sections)
+        },
+        update: function (callback) {
+            updators.push(callback)
         }
     }
+}
+])
+
+app.controller('ToolbarController', ['$scope', 'SectionService', function ($scope, SectionService) {
+    $scope.sections = SectionService.getSections()
+    $scope.section = 0
+    $scope.problem = 0
+    SectionService.update(function () {
+        $scope.$apply()
+    })
 }])
 
 app.controller('HomePageController', ['$scope', '$timeout', 'SectionService', function ($scope, $timeout, SectionService) {
@@ -76,7 +133,7 @@ app.controller('ProblemController', ['$scope', '$sce', function ($scope, $sce) {
                 $scope.$apply(function () {
                     try {
                         $scope.problem.description = $sce.trustAsHtml(markdown.toHtml(contents.overview))
-                    } catch(e) {
+                    } catch (e) {
                         $scope.problem.description = $sce.trustAsHtml(contents.overview)
                     }
                     $scope.code = 'class ' + (contents.filename.split('.')[0]) + ' {\n    \n}'
